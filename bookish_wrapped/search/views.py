8890e8e10dc804c7c23fc24cae394965f3f7b72c
Jeff Wheeler <jeffwheeler95@gmail.com>
2024-06-13 18:25:53 -0400

    implemented search by title in django

--- bookish_wrapped/search/views.py
+++ bookish_wrapped/search/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.http import HttpResponse
+
+
+import requests
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Hello, world. You're at the search index.")
+
+# Example of a working GET call
+def titleSearch(request, title):
+    url = f"https://openlibrary.org/search.json?title={title}"
+    response = requests.get(url)
+    data = response.json()
+    return JsonResponse(data)
